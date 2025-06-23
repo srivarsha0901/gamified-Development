9d5f0fcd81c39edf8bbd29cc5d05dfbcbf5fe4bf
Shalini0810 <shalinikotha08@gmail.com>
2025-06-23 11:44:14 +0530

    added tailwind css for home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-
+import './App.css'
 function App() {
   return (
     <Router>
