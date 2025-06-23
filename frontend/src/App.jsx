4c9fbeb239037a7b9ef4905ae496625c50487585
Shalini0810 <shalinikotha08@gmail.com>
2025-06-23 12:17:11 +0530

    added backend routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import AdminLogin from "./pages/AdminLogin";
 import './App.css'
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/admin/login" element={<AdminLogin />} />
       </Routes>
     </Router>
   );
